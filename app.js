27284e6f56811b0b131a817b1af685861d11d6f5
chinmay234 <chinmay@7dliving.com>
2024-04-19 16:25:12 +0530

    Modified Code

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware to enable CORS
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // Endpoint to serve model.json
 app.get("/model.json", (req, res) => {
